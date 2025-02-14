46550c6d5f7704c2648f1a7dff054cb019d475b4
Gi <160989051+Giolii@users.noreply.github.com>
2025-02-13 18:28:10 -0800

    Update cors options links

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const corsOptions = {
   origin: [
     process.env.VITE_FRONTEND_URL,
     process.env.VITE_FRONTEND_URL2,
-    "http://localhost:5173/",
-    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://localhost:5174",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
